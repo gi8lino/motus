df277010ddd70436d14e2e142675b9c5783420d6
gi8 <giottolino@gmail.com>
2026-02-03 23:51:33 +0100

    fix: protect /users endpoint

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -48,7 +48,9 @@ func NewRouter(
 	apiMux.Handle("POST /login", api.Login())
 	apiMux.Handle("PUT /me/password", api.ChangePassword())
 	apiMux.Handle("PUT /me/name", api.UpdateUserName())
-	apiMux.Handle("GET /users", api.GetUsers())
+	apiMux.Handle("GET /users",
+		middleware.Chain(api.GetUsers(), middleware.RequireAdmin(api.Users, api.AuthHeader)),
+	)
 	apiMux.Handle("POST /users", api.CreateUser())
 	apiMux.Handle("PUT /users/{id}/admin",
 		middleware.Chain(api.UpdateUserRole(),
