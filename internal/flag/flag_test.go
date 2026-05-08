ce67aa365b004540bb4c196613211364df2db26d
gi8 <giottolino@gmail.com>
2026-05-08 16:46:27 +0200

    fix: test case

--- internal/flag/flag_test.go
+++ internal/flag/flag_test.go
@@ -122,7 +122,7 @@ func TestParseFlags(t *testing.T) {
 		_, err := ParseFlags(args, "")
 
 		assert.Error(t, err)
-		assert.EqualError(t, err, "unknown flag: --invalid")
+		assert.EqualError(t, err, "unknown flag --invalid")
 	})
 
 	t.Run("missing database url", func(t *testing.T) {
@@ -149,7 +149,7 @@ func TestParseFlags(t *testing.T) {
 		_, err := ParseFlags(args, "")
 
 		assert.Error(t, err)
-		assert.EqualError(t, err, "invalid value for flag --log-format: must be one of: text, json.")
+		assert.EqualError(t, err, "invalid value for flag --log-format: must be one of: text, json")
 	})
 
 	t.Run("test route prefix", func(t *testing.T) {
